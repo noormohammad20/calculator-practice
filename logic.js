465a034e98fdf389fcb21050e96d5a64d3a31338
noormohammad20 <mdsaim204@gmail.com>
2022-02-13 13:58:14 +0600

    almost done

--- logic.js
+++ logic.js
@@ -1,9 +1,26 @@
 let screen = document.getElementById('screen')
 buttons = document.querySelectorAll('button')
+let screenValue = '';
 for (button of buttons) {
     button.addEventListener('click', (e) => {
         buttonText = e.target.innerText
         console.log(buttonText)
+        if (buttonText == ('x')) {
+            buttonText = "*"
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == ('c')) {
+            screenValue = ''
+            screen.value = screenValue;
+        }
+        else if (buttonText == ('=')) {
+            screen.value = eval(screen.value)
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
 
 
     })
